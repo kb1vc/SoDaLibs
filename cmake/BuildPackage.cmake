9ee4e36ac2fbff133ea82d251a82eed0fed12bee
Matthew Reilly <kb1vc@kb1vc.org>
2025-07-05 20:37:39 -0400

    Ubuntu package build and test apparently works.

--- cmake/BuildPackage.cmake
+++ cmake/BuildPackage.cmake
@@ -82,12 +82,5 @@ IF( EXISTS "${CMAKE_ROOT}/Modules/CPack.cmake" )
 
 INCLUDE( CPack )
 
-message(STATUS "\n\n\n\n-----------------\n")
-get_cmake_property(_varNames VARIABLES)
-list(SORT _varNames)
-foreach(_varName ${_varNames})
-  message(STATUS "${_varName}=${${_varName}}")
-endforeach()
-
 
 ENDIF()
